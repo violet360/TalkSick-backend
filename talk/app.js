73e76de9b3f13e3d458de0e939a211eafcee6083
violet360 <mazedk2k@gmail.com>
2021-05-03 23:13:38 +0530

    >emitting network info inaccordance to room user chose

--- talk/app.js
+++ talk/app.js
@@ -1,26 +1,28 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
-    cors: {
-        origins: ['http://localhost:4200']
-    }
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origins: ["http://localhost:4200"],
+  },
 });
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hey Socket.io</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>gimme mars........bar!!</h1>");
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('my message', (msg) => {
-        console.log(msg)
-    })
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  let { handle, room } = socket.handshake.auth;
+  socket.join(`${room}`);
+  socket.emit("networkInfo", `Welcome to ${room}`);
+  // socket.broadcast.emit("networkInfo", `${handle} has joined the room`);
+  socket.to(`${room}`).emit("networkInfo", `${handle} has joined the room`); //to all clients except the sender
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    io.in(`${room}`).emit("networkInfo", `${handle} left :(`);
+  });
 });
 
 http.listen(3000, () => {
-    console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
